52b0cc088a94de0aac29337354bbba41c77b5379
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-07-04 13:36:56 +0300

    Complete exercise 2.1

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,22 +8,24 @@ function Toggle({ children }) {
   const [on, setOn] = React.useState(false);
   const toggle = () => setOn(!on);
 
-  return React.Children.map(children, child =>
-    React.cloneElement(child, { on, toggle }),
-  );
+  return React.Children.map(children, (child) => {
+    return typeof child.type === "string"
+      ? child
+      : React.cloneElement(child, { on, toggle });
+  });
 }
 
 function ToggleOn({ on, children }) {
   return on ? children : null;
-};
+}
 
 function ToggleOff({ on, children }) {
   return on ? null : children;
-};
+}
 
 function ToggleButton({ on, toggle, ...props }) {
   return <Switch on={on} onClick={toggle} {...props} />;
-};
+}
 
 function App() {
   return (
@@ -32,6 +34,7 @@ function App() {
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
         <ToggleButton />
+        <span>Hello</span>
       </Toggle>
     </div>
   );
